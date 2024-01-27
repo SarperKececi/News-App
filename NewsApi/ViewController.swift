a319aee809ead3e9166c39f1e3553ce5bc86dd49
GitHub Kullanıcı Adınız <GitHub E-Posta Adresiniz>
2024-01-27 14:39:46 +0300

    Initial Commit

---
+++ NewsApi/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewsApi
+//
+//  Created by Sarper Kececi on 27.01.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
